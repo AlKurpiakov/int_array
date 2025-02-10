3cc43cb0f56b6973630b15f55e6ce63ec8867734
AlKurpiakov <s23380350@unn.ru>
2025-02-10 14:22:28 +0300

    asd

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <forward_list>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+
+
+int main(){
+    vector<int> vec = {1,2,3,4,5,6,7};
+
+    std::forward_list<int> list = {1,2,3,4,5,6,7};
+    for (auto it = vec.begin(); it != vec.end(); it++){
+        cout << *(it) << endl;
+    }
+
+    
+    std::sort(vec.begin(), vec.end());
+
+    for (auto it : vec){
+        cout << (it) << endl;
+    }
+
+    return 0;
+}
